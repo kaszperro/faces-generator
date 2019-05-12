c09186dce0e5578225f9a341c61842e4af60879a
Kasper Sapała <kasper.sapala@gmail.com>
2019-05-12 22:56:05 +0200

    Interactive Generator App update

--- Evaluation.py
+++ Evaluation.py
@@ -5,17 +5,30 @@ import torch
 from Models import get_generator_from_file
 
 
-def gen_random_image(model_path):
+def generate_image_from_vector(input_vector, model_path='./trained/generator.pth'):
+    """
+
+    :param model_path: path to model
+    :param input_vector: vector of 100 numbers
+    :return: image as numpy array of shape (3, 64, 64)
+    """
     generator = get_generator_from_file(model_path)
-    noise = torch.randn(1, 100, 1, 1)
+
+    noise = torch.tensor(input_vector, dtype=torch.float32).unsqueeze(0).unsqueeze(2).unsqueeze(2)
 
     out_image = generator(noise).detach().cpu()[0]
 
-    plt.imshow(np.transpose(out_image, (1, 2, 0)))
+    return np.transpose(out_image, (1, 2, 0))
+
+
+def display_random_image(model_path='./trained/generator.pth'):
+    noise = np.random.rand(100)
+
+    out_image = generate_image_from_vector(noise, model_path)
+
+    plt.imshow(out_image)
     plt.show()
 
 
 if __name__ == '__main__':
-    generator_path = './trained/generator.pth'
-
-    gen_random_image(generator_path)
+    display_random_image()
---
+++ GeneratorApp.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+
+import matplotlib
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+from Evaluation import generate_image_from_vector
+
+
+class App:
+    def __init__(self, num_sliders):
+        self.root = tk.Tk()
+        self.num_sliders = num_sliders
+        self.slider_values = np.random.rand(self.num_sliders)
+        self.generate_sliders()
+        self.noise = None
+        self.generate_noise()
+
+        image = generate_image_from_vector(self.noise)
+        f = Figure(figsize=(5, 5), dpi=100)
+        a = f.add_subplot(111)
+        self.face_image = a.imshow(image)
+        self.canvas = FigureCanvasTkAgg(f, self.root)
+        self.canvas.get_tk_widget().grid(row=0, column=1, rowspan=num_sliders)
+
+        self.generate_image()
+        self.root.mainloop()
+
+    def generate_sliders(self):
+        for i in range(self.num_sliders):
+            slider = tk.Scale(
+                self.root,
+                from_=-1.0,
+                to=1.0,
+                resolution=0.01,
+                orient=tk.HORIZONTAL
+            )
+            slider.set(self.slider_values[i])
+            slider.grid(row=i, column=0)
+            slider.bind("<B1-Motion>", lambda x, s=slider, index=i: self.slider_val_changed(index, s))
+
+    def slider_val_changed(self, index, slider):
+        self.slider_values[index] = slider.get()
+        self.generate_noise()
+        self.generate_image()
+
+    def generate_noise(self):
+        self.noise = np.random.rand(100)
+        for i in range(self.num_sliders):
+            self.noise[i * 100 // self.num_sliders:(i + 1) * 100 // self.num_sliders] = self.slider_values[i]
+
+    def generate_image(self):
+        image = generate_image_from_vector(self.noise)
+        self.face_image.set_data(image)
+        self.canvas.draw()
+
+
+def main():
+    matplotlib.use("TkAgg")
+    App(10)
+
+
+if __name__ == '__main__':
+    main()
