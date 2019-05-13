images normalization fix

Evaluation.py
@@ -18,7 +18,9 @@ def generate_image_from_vector(input_vector, model_path='./trained/generator.pth
 
     out_image = generator(noise).detach().cpu()[0]
 
-    return np.transpose(out_image, (1, 2, 0))
+    out_image = np.transpose(out_image, (1, 2, 0))
+
+    return out_image * 0.5 + 0.5  # undo normalization
 
 
 def display_random_image(model_path='./trained/generator.pth'):

Models.py
@@ -65,6 +65,6 @@ class Discriminator(nn.Module):
 def get_generator_from_file(file_path):
     num_input = 100
     device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-    gen = Generator(num_input=num_input).to(device)
-    gen.load_state_dict(torch.load(file_path))
+    gen = Generator(num_input=num_input, ).to(device)
+    gen.load_state_dict(torch.load(file_path, map_location='cpu'))
     return gen.eval()